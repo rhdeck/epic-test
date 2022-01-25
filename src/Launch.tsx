827cac4d29a88b399155c4d09629f942eeea67db
Ray Deck <ray@raydeck.com>
2022-01-25 11:29:27 -0500

    feat: pause the launch for easier inspection

--- src/Launch.tsx
+++ src/Launch.tsx
@@ -1,14 +1,24 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { oauth2 } from "fhirclient";
 function Launch() {
-  const scope = [].join(" ");
-  const clientId = "b9e5759a-2c88-4592-b9d5-46f30b744754";
+  const scope = ["launch/encounter"].join(" ");
+  const clientId = "b9e5759a-2c88-4592-b9d5-46f30b744754"; //Client 1
+  //   const clientId = "c2cfbf30-dbc3-44fd-9434-a39dcc3bd525";
+  const [uri, setUri] = useState("");
   useEffect(() => {
-    oauth2.authorize({ clientId, scope, noRedirect: true }).then((ret) => {
-      console.log("ret is ", ret);
-    });
+    oauth2
+      .authorize({
+        client_id: clientId,
+        scope,
+        noRedirect: true,
+        redirectUri: "/",
+      })
+      .then((ret) => {
+        console.log("ret is ", ret);
+        if (ret) setUri(ret);
+      });
   }, [scope, clientId]);
-  return null;
+  return <a href={uri}>Launch</a>;
 }
 
 export default Launch;
